66b1bbcc4e4b5a1932aeb799e9e901bea5f2cbe5
edanu <edanurgider94@hotmail.com>
2024-04-02 14:32:17 +0300

    02.04.2024         14:32

---
+++ Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static  void main ( String[] args) {
+        /* Karşılaştırma Operatörleri
+
+
+         ==  --> Eşit mi?
+         !=  --> Eşit Değil mi?
+         >  --> Büyük mü?
+         >=  --> Büyük veya Eşit mi?
+         <  --> Küçük mü?
+         <=  --> Küçük veya Eşit mi?
+
+
+         */
+
+
+        System.out.println(3 ==3 );  //  3 3 e eşitmi diye soruyoruz. sonuc true gelir. Boolean veri tipi olarak.
+        System.out.println(3==4);
+        System.out.println( 3!=4);
+        System.out.println(4> 2);
+        System.out.println(2 > 5);
+        System.out.println( 2 >= 1);
+        System.out.println(2 >=2);
+        System.out.println(2<3);
+        System.out.println(2<=2);
+
+        //Stringde karsılastırabilirz
+
+        System.out.println("Eda" == "Eda");
+
+
+
+
+    }
+}
